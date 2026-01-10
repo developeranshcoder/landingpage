093fa1049ef64e28544b51bababe33ee9a26bb52
Ansh Gochar <anshgurjar331@gmail.com>
2026-01-10 18:39:21 +0530

    first commit

---
+++ localtogithub.sh
@@ -0,0 +1,7 @@
+echo "# landingpage" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/developeranshcoder/landingpage.git
+git push -u origin main
